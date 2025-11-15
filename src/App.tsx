eab0494a381288ca1de99ad44711304ba04732fa
ashuksu <ashuksu@gmail.com>
2025-11-15 08:19:00 +0100

    feat: implement routing and navigation with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import {Link, Route, Routes} from "react-router";
+import {clsx} from "clsx";
+
 export function App() {
+    const classNameLink = 'hover:text-amber-300 transition-colors';
+
     return (
         <>
-            Home
+            <nav className='flex items-center gap-3 p-3'>
+                <Link className={clsx(classNameLink)} to={'/'}>Home</Link>
+                <Link className={clsx(classNameLink)} to={'/about'}>About</Link>
+                <Link className={clsx(classNameLink)} to={'/users'}>Users</Link>
+            </nav>
+            <main className={'p-3 prose'}>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/about'} element={<About/>}/>
+                    <Route path={'/users'} element={<Users/>}/>
+                </Routes>
+            </main>
         </>
     )
+}
+
+function Home() {
+    return <h1>Home</h1>
+}
+
+function About() {
+    return <h1>About</h1>
+}
+
+function Users() {
+    return <h1>Users</h1>
 }
