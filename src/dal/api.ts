742dfeae1c6ed4b4d6b99448e68a2f4e4deaf43e
ashuksu <ashuksu@gmail.com>
2025-12-20 14:51:23 +0100

    refactor: streamline API key headers in fetch requests and tidy up usePageTitle import

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -36,12 +36,13 @@ export type GetTrackListOutput = { data: Track[] }
 const PAGE_SIZE = (value?: number) => value ? `?pageSize=${value}` : '';
 const TRACKS_URL = `${API_URL}/playlists/tracks`;
 const LIMITED_TRACKS_URL: string = TRACKS_URL + PAGE_SIZE(5); // 5 tracks per page
+const headers = {
+    'api-key': API_KEY
+}
 
 export function getTracks(): Promise<GetTrackListOutput> {
     return fetch(LIMITED_TRACKS_URL, {
-        headers: {
-            'api-key': API_KEY
-        }
+        headers: headers
     }).then(res => {
         if (!res.ok) throw new Error(`HTTP error! status (Track List): ${res.status}`);
         return res.json();
@@ -51,9 +52,7 @@ export function getTracks(): Promise<GetTrackListOutput> {
 
 export function getTrack(trackId: string): Promise<GetTrackDetailsOutput> {
     return fetch(TRACKS_URL + '/' + trackId, {
-        headers: {
-            'api-key': API_KEY
-        }
+        headers: headers
     }).then(res => {
         if (!res.ok) throw new Error(`HTTP error! status (Track Detail): ${res.status}`);
         return res.json();
